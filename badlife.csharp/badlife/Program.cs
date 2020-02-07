bb1e5bfbf14d6664c1d08f442bbefc981ea8f27b
morrisonst <60792193+morrisonst@users.noreply.github.com>
2020-02-07 18:09:48 +0000

    Initial Version

---
+++ badlife.csharp/badlife/Program.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace badlife
+{
+    /// <summary>
+    /// Implements Conway's Game Of Life badly
+    /// https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life on a torus
+    /// </summary>
+    public class Program
+    {
+        private static bool IsToroidalBoard = true;
+
+        private const char IsLiveCell = '*';
+        private const char IsDeadCell = '_';
+
+        //private static readonly string filePath = "sample_bad.txt";
+        private static readonly string filePath = "sample_input.txt";
+        //private static readonly string filePath = "sample_toad.txt";
+        //private static readonly string filePath = "sample_blinker.txt";
+        //private static readonly string filePath = "sample_glider.txt";
+
+        private static IEnumerable<string> ReadGameData(string filePath)
+        {
+            var rows = new List<string>();
+
+            try
+            {
+                using (var input = new StreamReader(filePath))
+                {
+                    string row;
+
+                    do
+                    {
+                        row = input.ReadLine();
+
+                        if (!string.IsNullOrWhiteSpace(row))
+                        {
+                            rows.Add(row);
+                        }
+
+                    } while (row != null);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error whilst reading file: {(string.IsNullOrWhiteSpace(filePath) ? "Not Specified" : filePath)}.", ex);
+            }
+
+            return rows;
+        }
+
+        private static void ValidateGameData(IEnumerable<string> data)
+        {
+            if (!data.Any(s => s.Contains(IsLiveCell.ToString()) || s.Contains(IsDeadCell.ToString())))
+            {
+                throw new ArgumentException("Game data is not in a recognized format.", nameof(data));
+            }
+        }
+
+        private static bool[][] InitialiseBoard(string[] data)
+        {
+            var board = new bool[data.Length][];
+
+            for (var row = 0; row < data.Length; row++)
+            {
+                board[row] = new bool[data[row].Length];
+
+                for (var cell = 0; cell < data[row].Length; cell++)
+                {
+                    board[row][cell] = (data[row][cell] == IsLiveCell);
+                }
+            }
+
+            return board;
+        }
+
+        private static int Main(string[] args)
+        {
+            try
+            {
+                Console.Clear();
+
+                var data = ReadGameData(filePath).ToArray();
+
+                ValidateGameData(data);
+
+                var board = InitialiseBoard(data);
+
+                Console.WriteLine("Press Escape key to finish or any other key to Evolve.");
+
+                var generation = 0;
+                do
+                {
+                    RenderBoard(generation, board);
+
+                    generation += 1;
+
+                    board = Evolve(board, IsToroidalBoard);
+
+                } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+                return 42; //Not sure about this return code...
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadLine();
+                return -1;
+            }
+        }
+
+        private static void RenderBoard(int generation, bool[][] boardData)
+        {
+            var columnCount =  boardData.Any()
+                ? boardData[0]?.Length
+                : 0;
+
+            var grid = new StringBuilder();
+
+            foreach (var row in boardData)
+            {
+                for (var cellIdx = 0; cellIdx < columnCount; cellIdx++)
+                {
+                    grid.Append(row[cellIdx] ? IsLiveCell : IsDeadCell);
+                }
+
+                grid.AppendLine();
+            }
+
+            Console.SetCursorPosition(0, 1); //Reset cursor position, better performance the Console.Clear().
+
+            Console.WriteLine($"Generation: {generation}"); //Track Generation.
+
+            Console.Write(grid.ToString());
+        }
+
+        private static bool[][] Evolve(bool[][] currentBoardState, bool isToroidal)
+        {
+            var rowCount = currentBoardState.Length;
+            var newBoardState = new bool[rowCount][];
+
+            if (newBoardState.Any())
+            {
+                var columnCount = currentBoardState[0].Length;
+
+                for (var row = 0; row < rowCount; row++)
+                {
+                    newBoardState[row] = new bool[columnCount];
+
+                    for (var cell = 0; cell < columnCount; cell++)
+                    {
+                        var liveNeighbors = GetLiveNeighborCount(row, cell, currentBoardState, isToroidal);
+
+                        //Apply the Game of Life Rules
+                        //Rules are:
+                        //1. Any live cell with two or three neighbors survives.
+                        //2. Any dead cell with three live neighbors becomes a live cell.
+                        //3. All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+
+                        newBoardState[row][cell] = currentBoardState[row][cell]
+                            ? !((liveNeighbors < 2) || (liveNeighbors > 3))
+                            : liveNeighbors == 3;
+                    }
+                }
+            }
+
+            return newBoardState;
+        }
+
+        /// <summary>
+        /// Gets the number of live neighbors in the 9 cell vicinity of a given cell.
+        /// </summary>
+        /// <param name="x">X-coordinate of the cell.</param>
+        /// <param name="y">Y-coordinate of the cell.</param>
+        /// <param name="board">The board.</param>
+        /// <param name="isToroidalBoard">Indicates if the board is on a torus or not.</param>
+        /// <returns>The number of live neighbors.</returns>
+        private static int GetLiveNeighborCount(int x, int y, bool[][] board, bool isToroidalBoard)
+        {
+            var liveNeighbors = 0;
+
+            var boardHeight = board.Length;
+
+            var boardWidth = board[0].Length;
+
+            for (var yOffset = -1; yOffset <= 1; yOffset++)
+            {
+                // If y + yOffset is off the board edge, and the board is not on a torus then continue.
+                if ((((y + yOffset) < 0) || ((y + yOffset) >= boardHeight)) && !isToroidalBoard)
+                {
+                    continue;
+                }
+
+                // Loop around the edges if y + yOffset is off the board.
+                var virtualY = (y + yOffset + boardHeight) % boardHeight;
+
+                for (var xOffset = -1; xOffset <= 1; xOffset++)
+                {
+                    // If  x + xOffset is off the board edge, and the board is not on a torus then continue.
+                    if (((x + xOffset < 0) || ((x + xOffset) >= boardWidth)) && !isToroidalBoard)
+                    {
+                        continue;
+                    }
+
+                    // Loop around the edges if x + xOffset is off the board.
+                    var virtualX = (x + xOffset + boardWidth) % boardWidth;
+
+                    // Count the neighbor cell at (h,k) if it is alive.
+                    if (board[virtualX][virtualY])
+                    {
+                        liveNeighbors += 1;
+                    }
+                }
+            }
+
+            // Decrement by 1 if (x,y) is alive since we counted it as a neighbor.
+            return liveNeighbors - (board[x][y] ? 1 : 0);
+        }
+    }
+}
---
+++ badlife.fsharp/badlife/Program.fs
@@ -0,0 +1,222 @@
+﻿// Implements Conway's Game Of Life badly
+// https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life on a torus
+
+open System
+open System.IO
+open System.Linq
+open System.Text
+
+let fileArgIdentifier : string = "file:"
+let torusArgIdentifier : string = "torus:"
+
+//let isToroidalBoard : bool = true
+let isLiveCell : char = '*'
+let isDeadCell : char = '_'
+
+//let filePath = "sample_input.txt";
+//let filePath = "sample_toad.txt";
+//let filePath = "sample_blinker.txt";
+//let filePath = "sample_glider.txt";
+
+// <summary>
+// Validates the supplied filePath.
+// </summary>
+let validateFilePath(filePath : string) : unit =
+    if(String.IsNullOrWhiteSpace(filePath)) then
+        raise (System.ArgumentException("Argument cannot be null or empty.", "filePath"))
+
+// <summary>
+// Reads the game data from a given file path.
+// </summary>
+let readGameDataAsSequence(filePath : string) = seq {
+    use input = new StreamReader(filePath)
+    while(not input.EndOfStream) do
+        yield input.ReadLine()
+}
+
+// <summary>
+// Validates and reads the game data from a given file path.
+// </summary>
+let readGameData(filePath : string) : string array =
+    try
+        validateFilePath(filePath)
+        readGameDataAsSequence(filePath).ToArray()
+    with
+    | :? System.ArgumentException as ex -> raise (System.Exception("Error whilst attempting to read file.", ex))
+    | :? System.Exception as ex -> raise (System.Exception( String.Concat("Error whilst attempting to read file: ", filePath, "."), ex))   
+
+// <summary>
+// Validates the game data.
+// </summary>
+let validateGameData(data : string array) : unit =
+    if(not (data.Any(fun s -> (s.Contains(isLiveCell.ToString()) || (s.Contains(isDeadCell.ToString())))))) then
+        raise (System.ArgumentException("Game data is not in a recognized format.", "data"))    
+        
+// <summary>
+// Gets the number of live neighbors in the 9 cell vicinity of a given cell.
+// </summary>
+let getLiveNeighbourCount (x : int, y : int, board : bool array array , isToroidalBoard: bool) : int = 
+    let mutable liveNeighbors = 0
+    let boardHeight = board.Length
+    let boardWidth = board.[0].Length
+    
+    //Check the row immediately above, the row itself and the row immediately below..
+    for yOffset in [-1 .. 1] do
+
+        // If y + yOffset is off the board edge, and the board is not on a torus then continue, otherwise.
+        if (not ((((y + yOffset) < 0) || ((y + yOffset) >= boardHeight)) && not isToroidalBoard)) then
+            
+            // Loop around the edges if y + yOffset is off the board.
+            let virtualY = (y + yOffset + boardHeight) % boardHeight
+            
+            //Check the column immediately to the left, the column itself and the column immediately to the right..
+            for xOffset in [-1 .. 1] do
+                
+                // If  x + xOffset is off the board edge, and the board is not on a torus then continue, otherwise.
+                if (not (((x + xOffset < 0) || ((x + xOffset) >= boardWidth)) && not isToroidalBoard)) then
+                    
+                    // Loop around the edges if x + xOffset is off the board.
+                    let virtualX = (x + xOffset + boardWidth) % boardWidth
+                    
+                    // Count the neighbor cell at (h,k) if it is alive.
+                    if (board.[virtualX].[virtualY]) then 
+                        liveNeighbors <- liveNeighbors + 1
+    
+    //Decrement by 1 if (x,y) is alive since we counted it as a neighbor.
+    if (board.[x].[y]) then
+        liveNeighbors <- liveNeighbors - 1
+    
+    liveNeighbors                    
+
+
+// <summary>
+// Initialises the board with the supplie data.
+// </summary>
+let initialiseBoard(data : string array) : bool array array =
+    //Initialise the new board with rows
+    let board : bool array array = Array.zeroCreate data.Length
+
+    for row in [0 .. data.Length-1] do
+        //Initialise the new set of columns
+        board.[row] <- Array.zeroCreate data.[row].Length
+        
+        for cell in [0 .. data.[row].Length-1] do
+            //Determines if the given cell at position (x,y) in the supplied data is 
+            //live or dead and sets the new board with the given value.
+            board.[row].[cell] <- data.[row].[cell] = isLiveCell
+
+    board
+
+// <summary>
+// Renders the game board with the supplied data.
+// </summary>
+let renderBoard(generation : int, boardData: bool array array) : unit =
+    let mutable columnCount = 0
+    
+    if(boardData.Any()) then
+        columnCount <- boardData.[0].Length
+    
+    //Use string builder as it is more efficient when concatenating strings.
+    let grid = new StringBuilder()
+    
+    for row in [0 .. boardData.Length-1] do
+        for cellIdx in [0 .. columnCount-1] do
+            
+            if(boardData.[row].[cellIdx]) then
+                grid.Append(isLiveCell) |> ignore
+            else
+                grid.Append(isDeadCell) |> ignore
+    
+        grid.AppendLine() |> ignore
+    
+    //Reset the command line cursor position as it is less expensive than doing a Console.Clear()
+    Console.SetCursorPosition(0, 1)
+        
+    printfn "Generation: %i" generation
+
+    Console.Write(grid.ToString())
+
+// <summary>
+// Evolves  the game state to the next generation.
+// </summary>
+let evolve (currentBoardState : bool array array, isToroidal: bool) : bool array array = 
+    let rowCount = currentBoardState.Length    
+    let newBoardState : bool array array = Array.zeroCreate rowCount
+        
+    if(currentBoardState.Any()) then
+        let columnCount = currentBoardState.[0].Length
+
+        for row in [0 .. rowCount-1] do
+            newBoardState.[row] <- Array.zeroCreate columnCount
+
+            for cell in [0 .. columnCount-1] do
+
+                let liveNeighbors = getLiveNeighbourCount(row, cell, currentBoardState, isToroidal)
+            
+                //Apply the Game of Life Rules
+                //Rules are:
+                //1. Any live cell with two or three neighbors survives.
+                //2. Any dead cell with three live neighbors becomes a live cell.
+                //3. All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+                if(currentBoardState.[row].[cell]) then 
+                    newBoardState.[row].[cell] <- (not((liveNeighbors < 2) || (liveNeighbors > 3)))
+                else 
+                    newBoardState.[row].[cell] <- (liveNeighbors = 3)
+    
+    newBoardState
+
+// <summary>
+// Validates the supplied command line arguments.
+// </summary>
+let validateArgs(args: string array) : bool = 
+    (args.Any(fun s -> (s.StartsWith(fileArgIdentifier) || (s.StartsWith(torusArgIdentifier)))))
+
+// <summary>
+// Displays command line arguments help message to user.
+// </summary>
+let showHelpMessage() : unit = 
+    printfn "Supported command arguments [file:<filename> torus:<true|false>]"
+
+// <summary>
+// Extracts the string value of the given command line argument identifier.
+// </summary>
+let extractArg(args: string array, identifier : string) : string =
+    args.FirstOrDefault(fun s -> (s.StartsWith(identifier))).Split(':').[1]
+
+
+[<EntryPoint>]
+let main(args) =
+    
+    Console.Clear()
+    if(validateArgs(args) = false) then
+        showHelpMessage()
+        -1
+    else
+        try
+            let filePath = extractArg(args, "file").ToLowerInvariant()
+            let isToroidalBoard = (extractArg(args, "torus").ToLowerInvariant() = bool.TrueString.ToLowerInvariant())
+
+            let data = readGameData(filePath).ToArray()
+            validateGameData(data)
+
+            let mutable board = initialiseBoard(data)
+
+            let mutable generation = 0
+
+            printfn "Press Escape key to finish or any other key to Evolve."
+
+            let uiLoop() =
+            
+                renderBoard(generation, board)
+            
+                generation <- generation + 1
+            
+                board <- evolve(board, isToroidalBoard)
+
+                not (Console.ReadKey(true).Key = ConsoleKey.Escape)
+
+            while uiLoop() do ignore None
+
+            42 // return an integer exit code
+        with
+        | :? System.Exception as ex -> Console.WriteLine(String.Format("An error occurred: => {0}.", ex.Message)); -1
